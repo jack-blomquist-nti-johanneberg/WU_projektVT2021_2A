740d8523456184d1de581aa9205fe2a4f87a7db6
jack-blomquist-nti-johanneberg <jack.blomquist@elev.ga.ntig.se>
2021-03-05 16:14:50 +0100

    startsida avklarad, problem med nav

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 
+// slide out nav menu
 const nav_button = document.querySelector(".nav_button")
 const nav_menu = document.querySelector(".nav_slider_menu")
-const sub_service_button = document.querySelector(".sub_service_toggle")
-const sub_service_menu = document.querySelector(".sub_service_menu")
 
 function toggle_nav_menu(){
     nav_menu.classList.toggle("nav_slider_menu_active")
+    sub_service_menu.classList.remove("sub_service_menu_active")
 }
 
 nav_button.addEventListener("click", toggle_nav_menu)
 
+// slide out service menu
+const sub_service_button = document.querySelector(".sub_service_toggle")
+const sub_service_menu = document.querySelector(".sub_service_menu")
+
 function toggle_sub_service_menu(){
     sub_service_menu.classList.toggle("sub_service_menu_active")
 }
-
+    
 sub_service_button.addEventListener("click", toggle_sub_service_menu)
